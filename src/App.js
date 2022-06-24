502f0769063958dc35aed9ede0661be9012b07c1
AagmanSuri <aagmansuri96@gmail.com>
2022-06-24 13:51:52 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Mockman from "mockman-js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Mockman
+        <Mockman />
+      </header>
+    </div>
+  );
+}
+
+export default App;
